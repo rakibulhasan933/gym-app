36094f17fe3f14a28f72e7556bb1d3098ba2a21c
RAKIBUL <rakibulmd933@gmail.com>
2022-07-08 23:58:27 +0600

    header

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -6,7 +6,11 @@ const Hero = () => {
     return (
         <div className="hero">
             <div className="left-h">
-               <Header/>
+                <Header />
+                <div className="the-best-ad">
+                    <div></div>
+                    <span>The Best Fitness Class in the Town</span>
+                </div>
             </div>
             <div className="right-h">
                 right
